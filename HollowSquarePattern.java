af306737546af1548f32eeb0ea36313d6439234f
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-05-03 17:53:21 +0530

    Patterns

---
+++ HollowSquarePattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class HollowSquarePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= m; b++) {
+                if (a == 1 || b == 1 || a == n || b == m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
