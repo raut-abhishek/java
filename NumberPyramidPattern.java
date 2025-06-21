4ece79969acf035e4b4e23950d81521227a90925
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-06-21 23:34:08 +0530

    Added new file "Number Pyramid Parrern"

---
+++ NumberPyramidPattern.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class NumberPyramidPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            // First spaces
+            int FirstSpaces = n - i;
+            for (int j = 1; j <= FirstSpaces; j++) {
+                System.out.print(" ");
+            }
+            // Numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+
+            }
+            System.out.println();
+        }
+    }
+}
