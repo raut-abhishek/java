355c78e339792767e4e1eef5a770d1467f4f5dde
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-06-21 21:03:31 +0530

    File added "Operators"

---
+++ Operators.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+        // Binary Operators
+        System.out.println("Binary Operation result");
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println(a * b);
+        System.out.println(a % b);
+        // Unary operators
+        System.out.println("Unary Operation result");
+        System.out.println(a++);
+        System.out.println(++a);
+        // Unary operators
+        System.out.println("Relational Operation result");
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        //Logical Operators 
+        if (a>b && b<a){    //Logical and (&&)
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+        if (a==b || b<a){    //Logical OR (||)
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+        if (!(a>b)){    //Logical Not (!)
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
