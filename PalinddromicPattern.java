b210b0bb4eae66fc5a684741b527e45e56b619d8
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-06-22 00:03:04 +0530

    Added new File "Palindromic Pyrimid Pattern"

---
+++ PalinddromicPattern.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class PalinddromicPattern {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i = 1; i<=n; i++){
+            //spaces
+            int spaces = n-i;
+            for(int j = 1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            //First half numbers
+            for(int j=i; j>=1; j--){
+                System.out.print(j);
+            }
+            //2nd half
+            for(int j=2; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
