aa44cfe8f14797dcc077e2f459d0bb4b55ff995f
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-06-22 23:00:08 +0530

    Added new file "Multiplication Function"

---
+++ MultiplicationFunction.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class MultiplicationFunction { 
+    public static void Multiplication(int a,int b){
+        System.out.println("Multiplication = "+(a*b));
+        return;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number = ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number = ");
+        int b = sc.nextInt();
+        Multiplication(a, b);
+
+    }
+}
