8caf15e727c125a7c5345dea6e255e93b4a2157d
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-06-22 23:30:31 +0530

    Added New File "Factorial Function"

---
+++ FactorialFunction.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class FactorialFunction {
+    public static void PrintFactorial(int a) {
+        if(a<0){
+            System.out.println("please enter a valid number");
+            return;
+            
+        }
+        int factorial = 1;
+        for (int i = a; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+        System.out.println("Factorial = " + factorial);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number = ");
+        int a = sc.nextInt();
+        PrintFactorial(a);
+    }
+}
