1adfbef8e8543bb0d16f32ef0f8c3f32e5ca3070
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-06-22 22:41:05 +0530

    Added new file "Addition Function"

---
+++ AdditionFinction.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class AdditionFinction {
+    public static void AdditionFunction(int a,int b){
+        System.out.println("Addition = "+(a+b));
+        return;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number = ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number = ");
+        int b = sc.nextInt();
+        AdditionFunction(a, b);
+
+    }
+}
