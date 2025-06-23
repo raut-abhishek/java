f8b0116ab8afb9a746c5146d4bc0ebb794b16508
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-06-23 14:43:46 +0530

    Added new file "Greater Of Two Numbers"

---
+++ GreaterOfTwoFunction.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class GreaterOfTwoFunction {
+    public static void GreaterOfTwo(int a, int b) {
+        if(a==b){
+            System.out.println("Both numbers are equal.");
+            return;
+        }
+        else if (a>b){
+            System.out.println(a+" is grater.");
+            return;
+        }
+        else{
+            System.out.println(b+" is grater.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number = ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number = ");
+        int b = sc.nextInt();
+        GreaterOfTwo(a, b);
+    }
+
+}
