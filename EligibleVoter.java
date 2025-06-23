b2e61e4be93a0eb69cd9f3e35308235cd972a2a5
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-06-23 15:33:28 +0530

    File added "Eligible Voter"

---
+++ EligibleVoter.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class EligibleVoter {
+    public static void EligibilityCalculator(int a){
+        if(a>=18){
+            System.out.println("You are eligible to vote.");
+        }
+        else{
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter age = ");
+        int a = sc.nextInt();
+        EligibilityCalculator(a);
+    }
+}
