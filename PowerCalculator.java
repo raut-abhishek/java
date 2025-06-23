2668e1af9993981d817391a9a59ed88633eb58d0
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-06-23 17:57:16 +0530

    File Added

---
+++ PowerCalculator.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class PowerCalculator {
+    public static void CalculatePower(int base, int exponent) {
+        double exponentiation = Math.pow(base, exponent);
+        System.out.print(base + " raised to the power of " + exponent + " is : " + exponentiation);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number : ");
+        int base = sc.nextInt();
+        System.out.print("Enter power : ");
+        int exponent = sc.nextInt();
+        CalculatePower(base, exponent);
+
+    }
+}
