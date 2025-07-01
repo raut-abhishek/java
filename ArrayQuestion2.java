aad3c93dd352c96b19e3f286db2657e2a76a7804
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-01 22:55:17 +0530

    Storing numbers in array

---
+++ ArrayQuestion2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ArrayQuestion2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many numbers you want to input: ");
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+        }
+        System.out.println("You entered:");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Number " + (i + 1) + ": " + numbers[i]);
+        }
+
+        sc.close();
+    }
+}
