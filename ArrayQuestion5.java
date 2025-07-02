51952a5d433de7ab3cbfca109048b97303ceddf5
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-02 23:13:08 +0530

    Find the maximum number in the array

---
+++ ArrayQuestion5.java
@@ -0,0 +1,18 @@
+
+// Find the maximum number in the array
+import java.util.*;
+
+public class ArrayQuestion5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many numbers you want to enter : ");
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+        for(int i = 0; i<n; i++){
+            System.out.print("Number "+(i+1)+" : ");
+            numbers[i] = sc.nextInt();
+        }
+        Arrays.sort(numbers);
+        System.out.print("The maximum number is ; " + numbers[n-1] + ".");        
+    }
+}
