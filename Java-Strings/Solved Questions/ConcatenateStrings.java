713fdb23357d2356de0afa7c03f5cbb1cf492c78
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-21 23:51:30 +0530

    Concatenate Strings

---
+++ Java-Strings/Solved Questions/ConcatenateStrings.java
@@ -0,0 +1,12 @@
+// Concatenate Strings
+// this basically means adding value of two strings to one string
+public class ConcatenateStrings {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Abhishek";
+
+        String result = str1 + " " + str2;                      // Using + operator
+
+        System.out.println("Concatenated String: " + result);
+    }
+}
