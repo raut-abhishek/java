3cac570cf217ec26c9a0e39584960946056935c7
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-08-10 19:18:19 +0530

    Inheritance

---
+++ OOP/Inheritance/inheritance1.java
@@ -0,0 +1,14 @@
+// Inheritance
+class Shape{
+    String color;
+}
+class Triangle extends Shape{
+
+}
+
+public class inheritance1{
+    public static void main(String[] args) {
+       Triangle t1 = new Triangle();
+       t1.color = "Red"; 
+    }
+}
