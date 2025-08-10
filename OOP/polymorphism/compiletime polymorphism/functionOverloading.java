fbce7a74508bea5b612fda8d05e0a727e7f51b1a
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-08-10 11:46:49 +0530

    compiletime polymorphism

---
+++ OOP/polymorphism/compiletime polymorphism/functionOverloading.java
@@ -0,0 +1,23 @@
+// compiletime polymorphism also known as function overloading
+class Student{
+    String name;
+    int age;
+
+    public void printInfo(String name){
+        System.out.println(name);
+    }
+    public void printInfo(int age){
+        System.out.println(age);
+    }
+    public void printInfo( String name, int age ){
+        System.out.println(name + " " + age);
+    }
+}
+public class functionOverloading{
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Abhishek";
+        s1.age = 23;
+        s1.printInfo(s1.name, s1.age);
+    }
+}
