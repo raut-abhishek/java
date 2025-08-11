515c55902f94e8c50324c27fec78b4fd4d0aedc5
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-08-11 12:57:50 +0530

    added a "diaplay" function

--- OOP/Inheritance/inheritance1.java
+++ OOP/Inheritance/inheritance1.java
@@ -3,12 +3,15 @@ class Shape{
     String color;
 }
 class Triangle extends Shape{
-
+    public void display(){
+        System.out.println("Color : " + color);
+    }
 }
 
 public class inheritance1{
     public static void main(String[] args) {
        Triangle t1 = new Triangle();
-       t1.color = "Red"; 
+       t1.color = "Red";
+       t1.display();
     }
 }
