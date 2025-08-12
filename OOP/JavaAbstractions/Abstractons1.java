645c00ee4014fb4d3baf683129f581ebe018b91f
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-08-12 21:06:02 +0530

    Abstraction

---
+++ OOP/JavaAbstractions/Abstractons1.java
@@ -0,0 +1,23 @@
+package JavaAbstractions;
+
+abstract class Animal{
+    abstract void walk();
+}
+
+class Horse extends Animal{
+    public void walk(){
+        System.out.println("Horse walks on 4 legs.");
+    }
+}
+
+class Chicken extends Animal{
+    public void walk(){
+        System.out.println("Chicken walks on 2 legs.");
+    }
+}
+
+public class Abstractons1 {
+    public static void main(String[] args) {
+        
+    }
+}
