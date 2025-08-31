816f40b13bb5583debcc809b730c3f9b4d1f75ef
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-09-01 01:24:47 +0530

    sum od all digits in a number

---
+++ DSA/recursion/sumOfDigits.java
@@ -0,0 +1,11 @@
+// sum od all digits in a number
+public class sumOfDigits {
+    public static int sumDigits(int n) {
+        if (n == 0) return 0;
+        return (n % 10) + sumDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumDigits(123));
+    }
+}
