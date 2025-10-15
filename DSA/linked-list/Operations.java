8cad61f6412080856895a5622b14a4b5af716e74
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-10-15 23:45:44 +0530

    created initial linked list

---
+++ DSA/linked-list/Operations.java
@@ -0,0 +1,37 @@
+// Operations on linked list 
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+
+    
+
+    void display() {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+}
+
+
+public class Operations {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.display();
+
+    }
+}
