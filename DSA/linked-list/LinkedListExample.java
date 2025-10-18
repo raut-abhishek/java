9352c23b15b1164cb7288baefab663a4ada5b69c
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-10-18 23:23:24 +0530

    linked list example

---
+++ DSA/linked-list/LinkedListExample.java
@@ -0,0 +1,57 @@
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    // We Add element at end of the list
+    void add(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    // Display our list
+    void print() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " > ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        list.print();
+    }
+}
+
+
+        
+    
+
