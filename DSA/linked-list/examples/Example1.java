58be254f13ce8b7f07e82df8f2914e8845d3924e
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-10-27 22:11:52 +0530

    Linked list example with add first, add last and print list functions

---
+++ DSA/linked-list/examples/Example1.java
@@ -0,0 +1,65 @@
+class Example1{
+    Node head;
+
+    class Node{
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }  
+
+    // add- first
+    public void addFirst(String data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // add last
+    public void addlast(String data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+
+        Node currNode = head;
+        while(currNode.next != null){
+            currNode = currNode.next;
+        }
+
+        currNode.next = newNode;
+        
+    }
+
+    // print list
+    public void printList(){
+        if(head == null){
+            System.out.println("List is empty");
+        }
+        Node currNode = head;
+        while(currNode != null){
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.print("NULL");
+
+    }
+
+    public static void main(String[] args){
+        Example1 list = new Example1();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.addFirst("This");
+        list.addlast("list");
+        list.printList();
+    }
+}
