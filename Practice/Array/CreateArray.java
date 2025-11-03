936b7865cf177871fa69bca5db36f715dd9a35af
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-11-03 19:46:20 +0530

    create display an array from users input

---
+++ Practice/Array/CreateArray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class CreateArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter size of the array : ");
+        int size= sc.nextInt();
+        int [] numbers = new int[size];
+        int a = 0;
+        while(a< numbers.length){
+            System.out.print("Enter " + a + ": ");
+            numbers[a] = sc.nextInt();
+            a++;
+        }
+        System.out.print("Your array is: ");
+        for(int i = 0; i< numbers.length; i++){
+            System.out.print(" ");
+            System.out.print(numbers[i]);
+        }
+        sc.close();
+        
+    }
+}
