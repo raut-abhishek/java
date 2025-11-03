2e5bb1717a23d588e4871cbad5cf4160e2cf7332
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-11-03 19:51:53 +0530

    loop on string characters

---
+++ Practice/Strings/LoopThroughString.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class LoopThroughString {
+    public static void main(String[] args) {
+        String name = "astronauts";
+        System.out.print("All letters are : ");
+        for (int i = 0; i < name.length(); i++) {
+            System.out.print(name.charAt(i) + " ");
+        }
+    }
+}
