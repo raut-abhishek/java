cf28d5b1396cf0ae9c8bd32bf11dde9e0cfbec79
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-11-19 23:45:07 +0530

    Bubble sort an array

---
+++ DSA/sorting/bubble-sort/AsscendingSort.java
@@ -0,0 +1,25 @@
+// Bubble sort an array
+
+public class AsscendingSort {
+
+    public static void printArray(int[] arr){
+        for(int n : arr){
+            System.out.print(n + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = { 7, 8, 3, 1, 2 };
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if(arr[j]> arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+
+    }
+}
