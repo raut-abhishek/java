93f88bd99cce4c7bb110a4f7bb6bbf1cfffb5c5f
raut-abhishek <iamabhishekraut01@gmail.com>
2025-11-26 23:43:16 +0530

    insertion sort an array

---
+++ DSA/sorting/insertion-sort/InsertionSortExample.java
@@ -0,0 +1,29 @@
+// insertion sort an array
+public class InsertionSortExample {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+
+        insertionSort(arr);
+
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+}
